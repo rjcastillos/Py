de62fa41925bf8d89326910f68ef3a093143fabd
Ramon Castillo <ramon@rcastillo.net>
2025-01-10 10:54:01 +0100

    fixes to warinings while creating new comulms and args passed when call as standalone and as function

--- invesments/trader/tools/getAtr.py
+++ invesments/trader/tools/getAtr.py
@@ -1,35 +1,40 @@
 
 #https://www.geeksforgeeks.org/different-ways-to-iterate-over-rows-in-pandas-dataframe/
+#from getAtr import *
+#For now only supports one TIcker at the time.
 import datetime
 
 Print=False
-OutputFile=True
+OutputFile=False
 Period=14
 tickersList="../Tickers_owned.json"
 
 
 def gATR(Tickers):
     print("received ARGS",Tickers)
+    Tickers=[Tickers]
+    #print("Tickers len =",len(Tickers))
     for Ticker in Tickers:
         print("->",Ticker)
         import yfinance as yf
         data=yf.download(Ticker)
         df=data.tail(Period+1)
-        print(df)
+        #print(df)
         import pandas as pd
         import numpy as py
         if OutputFile:
             df.to_csv('ATR_tmpout.csv')
         for i in range(len(df)):
             if i == 0:
-                print("First Row",df.iloc[i])
+                #print("First Row",df.iloc[i])
                 High=float(0)
                 Low=float(0)
                 pClose=float(0)
                 pC=0
-                df['HLD']=float(0)
-                df['HPC']=float(0)
-                df['LPC']=float(0)
+                df.insert((len(df.columns)) , "HLD" , [py.nan]*len(df))
+                df.insert((len(df.columns)) , "HPC" , [py.nan]*len(df))
+                df.insert((len(df.columns)) , "LPC" , [py.nan]*len(df))
+                df.insert((len(df.columns)) , "tr" , [py.nan]*len(df))
             while i > 0:
                 
                 High=df.iat[i,1]
@@ -37,23 +42,29 @@ def gATR(Tickers):
                 pC=i-1
                 pClose=df.iat[pC,0]
                 HLD=High-Low
+                #assigning values to new columns
                 df.iat[i,5]=HLD
                 HPC=abs(High-pClose)
                 df.iat[i,6]=HPC
                 LPC=abs(Low-pClose)
                 df.iat[i,7]=LPC
+                #sorting values to get the grater of HLD,HPC and LPC
+                values=[HLD,HPC,LPC]
+                values.sort()
+                #assigning the greater to new column tr
+                df.iat[i,8]=values[2]
                 _High=f"{High:.2f}"
                 _Low=f"{Low:.2f}"
                 _pClose=f"{High:.2f}"
                 _HLD=f"{HLD:.2f}"
                 _HPC=f"{HPC:.2f}"
                 _LPC=f"{LPC:.2f}"
-                print("High :",_High,"Low :",_Low,"pClose =",_pClose,"HLD = ",_HLD,"HPC =",_HPC,"LPC = ",_LPC)
+                #print("High :",_High,"Low :",_Low,"pClose =",_pClose,"HLD = ",_HLD,"HPC =",_HPC,"LPC = ",_LPC)
                 break
         #df.drop([0,1])
-        df['tr']=df[['HLD','HPC','LPC']].max(axis=1)
-        print("DATA FRAME with NEW COLS")
-        print(df)
+        #df['tr']=df[['HLD','HPC','LPC']].max(axis=1)
+        #print("DATA FRAME with NEW COLS")
+        #print(df)
         fATR=df['tr'].sum()
         ATR=f"{fATR/(len(df)-1):.2f}"
         print("<<<<< ATR >>>>> =",ATR)
