6db3aba0665a338db0ff024b86fffdb341326cc8
Ramon Castillo <ramon@rcastillo.net>
2025-01-30 20:51:59 +0100

    new fibcal version updating the 2018 one

---
+++ invesments/trader/tools/fibcalc.py
@@ -0,0 +1,93 @@
+#!/usr/local/bin/python3.6
+#on Mac /usr/local/bin/python3.6
+#Or Native /usr/bin/python
+##################
+#RC Oct 2018
+#Program_name: Fib
+#
+#Usage :   fib
+#       Fibonacci Calculator
+#       Calculates Retracements and Extensions in UP and DOWN trends
+#   https://www.investing.com/tools/fibonacci-calculator
+#lastest ver. 0.02
+#2025 01 30
+#changed to ba a  simpler function accepting 2 params
+# 2018 11 29 ver 0.02
+#       adapts DecPlaces() from q_buycal_00_08.py
+#
+#2018 10 26
+# 01    Implements simple Up trend Fibonacci:
+#       given a Low number (Low=A_) and a high (high=B_) one returns:
+#   Fib levels (Fib)
+#1) 23.6%
+#2) 38.2%
+#3) 50%
+#4) 61.8%
+#5) 65%
+#6) 76.4%
+###      Formula high-(high-Low)*Fib
+#
+#
+####################
+#   Import section #
+####################
+#Import
+import sys
+from pprint import pprint
+from TradeGoals_9 import *
+DEBUG=False
+ARGS={'-A':0.1,'-B':100.00,'-C':0.0,'-F':0.382}
+#
+#Functions
+#
+def Usage():
+        print ("Usage : {Num A} -- Mandatory")
+        print ("        {Num B} -- Mandatory")
+        quit()
+def DecPlaces(MyP):
+        DecIn=str(MyP)
+        print("Price str =",DecIn)
+        Dec=DecIn.split('.')
+        MyNumDec=5
+        if int(MyP) > 1000 :
+            if len(Dec) ==1:  ## Passeed is 99 therefore  Default to .2f
+                print("Price ${} does not contains decimals >> assiging .2f by Default".format(MyP))
+            elif len(Dec) ==2:      ## Passed is 99.9 0r more 99.999999
+                if len(Dec[1]) < 3 : MyNumDec=2  ## Passed is 99.9 or 99.99 Defaults to .2f
+        print("Price ${} will contain {} decimals places".format(MyP,MyNumDec))
+        return MyNumDec
+FibUP={'TwoThreeSix':0.236,'ThreeEightyTwo':0.382,'Fifty':0.5,'SixOneEight':0.618,'GoldenPocket':0.65,'Seven86':0.786}
+FibDown={}
+#    print("Price received =",price)
+A_=5755
+B_=8506.7
+#
+#
+#   PROGRAM
+#
+#Analizing sys.argv to set variables according with program's options
+#
+def FibCalc(_Low,_High):
+    A_=_Low
+    B_=_High
+    print("\nFib Retracements UPTrend from  ${0:,.{1}f} to  ${2:,.{1}f} ".format(A_,2,B_))
+    print("========================================================","\n")
+    print("{0:20s}  Fib Up =    ${1:>20,.{2}f}".format("aHundred",B_,2))
+    for k,v in FibUP.items():
+        print("{0:20s}  Fib Up =    ${1:>20,.{2}f}".format(k,B_-(B_-A_)*v,2)) ### high-(high-Low)*Fib
+    print("{0:20s}  Fib Up =    ${1:>20,.{2}f}".format("Zero",A_,2))
+
+def main():
+    if len(sys.argv) < 3:
+        Usage()
+    else:
+        A_=float(sys.argv[1])
+        B_=float(sys.argv[2])
+        FibCalc(A_,B_)
+
+#
+####
+# MAIN
+###      
+if __name__ == "__main__":
+    main()
